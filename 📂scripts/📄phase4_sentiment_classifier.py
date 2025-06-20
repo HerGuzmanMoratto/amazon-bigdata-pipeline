d88a21c67ea32dc36e55abbf615a2d60bf4b4d73
Hernan Guzman <herguzman@hotmail.es>
2025-06-20 03:14:34 +0200

    Create 📄phase4_sentiment_classifier.py

---
+++ 📂scripts/📄phase4_sentiment_classifier.py
@@ -0,0 +1,3 @@
+# Sentiment Classification analysis:
+
+# Steps performed to upload, convert and transform the columns to feed the Random Forest Classifier, Logistic Regression, and Decision Tree Classifier.
